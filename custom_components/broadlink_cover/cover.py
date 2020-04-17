3022a96e203c7acf3467020ae48cb1bfe2cd7cd0
EricHour <24457241+zoranke@users.noreply.github.com>
2020-04-18 04:29:18 +0800

    Update cover.py

--- custom_components/broadlink_cover/cover.py
+++ custom_components/broadlink_cover/cover.py
@@ -19,7 +19,7 @@ from homeassistant.helpers.restore_state import RestoreEntity
 from configparser import ConfigParser
 from base64 import b64encode, b64decode
 
-REQUIREMENTS = ['broadlink==0.12.0']
+REQUIREMENTS = ['broadlink==0.13.0']
 
 _LOGGER = logging.getLogger(__name__)
 
