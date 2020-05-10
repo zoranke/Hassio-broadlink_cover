06ad8665ef6286cf1ee9d918c12f32cfd33827c0
EricHour <24457241+zoranke@users.noreply.github.com>
2020-05-10 14:27:08 +0800

    Update cover.py

--- custom_components/broadlink_cover/cover.py
+++ custom_components/broadlink_cover/cover.py
@@ -336,11 +336,11 @@ class RMCover(CoverDevice,RestoreEntity):
             return self._auth(retry-1)
         return auth
 
-    async def async_added_to_hass(self):
-        await super().async_added_to_hass()
-        last_state = await self.async_get_last_state()
-        
-        if last_state:
-           self._position = last_state.attributes['current_position']
-        else:
-           self._position = None
+#    async def async_added_to_hass(self):
+#        await super().async_added_to_hass()
+#        last_state = await self.async_get_last_state()
+#        
+#        if last_state:
+#           self._position = last_state.attributes['current_position']
+#        else:
+#           self._position = None
